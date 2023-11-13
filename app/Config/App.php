3eb8ec5c8cfb9d28a67d8d94c7c6d0c51c21615d
vhynts <vhynts@gmail.com>
2023-11-13 21:50:13 +0700

    setting base url

--- app/Config/App.php
+++ app/Config/App.php
@@ -16,7 +16,7 @@ class App extends BaseConfig
      *
      *    http://example.com/
      */
-    public string $baseURL = 'http://localhost/iapotek/';
+    public string $baseURL = 'http://localhost/frontend-iapotek/';
 
     /**
      * Allowed Hostnames in the Site URL other than the hostname in the baseURL.
